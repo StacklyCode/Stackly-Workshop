06780faa126f7471ad7e5e065eeaf4ded1254eeb
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2021-02-02 08:27:45 -0500

    fix: change the title

--- react-part-1/webpack/src/index.js
+++ react-part-1/webpack/src/index.js
@@ -5,8 +5,8 @@ import img from "./logo.png";
 
 import "./index.css";
 
-const Image = <img src={img} alt="" />;
-const Title = <h1>Hello To React</h1>;
+const Image = <img src={img} alt="Logo" />;
+const Title = <h1>React Workshop | Webpack</h1>;
 
 const rootElement = document.getElementById("root");
 
